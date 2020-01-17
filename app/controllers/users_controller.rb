695516ec9e7a46fc4e55a6d60e86a2e182739ea7
IBUNHABIB <ibunhabib@gmail.com>
2020-01-17 18:26:09 +0300

    created the post model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Your account is created successfully'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Your account is updated successfully'
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
