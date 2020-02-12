01b7e1ab90b4e59c10b4d51c47e69771bd96bc33
EC2 Default User <ec2-user@ip-172-31-10-226.us-east-2.compute.internal>
2020-02-12 16:32:12 +0000

    listing all users with pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
   end
